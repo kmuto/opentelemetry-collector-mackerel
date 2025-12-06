99414b2eec51fcf885c09ca64773564cbf72eeb0
Kenshi Muto <kmuto@kmuto.jp>
2025-12-06 11:34:47 +0900

    replace

--- distributions/otelcol-mackerel/postinstall-debian.sh
+++ distributions/otelcol-mackerel/postinstall-debian.sh
@@ -3,29 +3,16 @@
 db_input high otelcol-mackerel/apikey || true
 db_go || true
 
-CONF=/etc/otelcol-mackerel/otelcol-mackerel.conf
-
 if [ "$1" = "configure" ]; then
-  if [ -f "$CONF" ]; then
-    exit 0
-  fi
-
   db_get otelcol-mackerel/apikey
   if [ "$RET" ]; then
-    mkdir -p /etc/otelcol-mackerel
-    cat > "$CONF" <<EOF
-# Systemd environment file for the otelcol-mackerel service
-
-# Command-line options for the otelcol-mackerel service
-OTELCOL_MACKEREL_OPTIONS="--config=mackerel:default"
-
-MACKEREL_APIKEY=$RET
-EOF
+    sed -i -e "/\# MACKEREL_APIKEY=your-api-key-here/c\MACKEREL_APIKEY=$RET" /etc/otelcol-mackerel/otelcol-mackerel.conf
   fi
-fi
 
-if [ "$1" = "configure" ]; then
   adduser --system --group --home /nonexistent --no-create-home --gecos "Mackerel OpenTelemetry Collector" --disabled-password --quiet otelcol-mackerel || true
+  chown otelcol-mackerel:otelcol-mackerel /etc/otelcol-mackerel /etc/otelcol-mackerel/otelcol-mackerel.conf
+  chmod 700 /etc/otelcol-mackerel
+  chmod 600 /etc/otelcol-mackerel/otelcol-mackerel.conf
 fi
 
 if command -v systemctl >/dev/null 2>&1; then
--- distributions/otelcol-mackerel/postremove-debian.sh
+++ distributions/otelcol-mackerel/postremove-debian.sh
@@ -3,8 +3,6 @@ set -e
 
 if [ "$1" = "purge" ]; then
   userdel otelcol-mackerel || true
-  rm -f /etc/otelcol-mackerel/otelcol-mackerel.conf
-  rmdir /etc/otelcol-mackerel || true
 
   if [ -e /usr/share/debconf/confmodule ]; then
     . /usr/share/debconf/confmodule
@@ -12,6 +10,6 @@ if [ "$1" = "purge" ]; then
   fi
 fi
 
-if [ "$1" = remove ] && [ -d /run/systemd/system ] ; then
+if [ "$1" = "remove" ] && [ -d /run/systemd/system ] ; then
         systemctl --system daemon-reload >/dev/null || true
 fi
